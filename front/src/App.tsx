68da93ba7491fe51eb6e4e917525e19555854076
Itamar Bar-Lev <bitamar@gmail.com>
2025-10-02 22:30:46 +0300

    sidebar

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,7 @@
-import { Route, Routes, Navigate } from 'react-router-dom';
-import { AppHeader } from './components/AppHeader';
-import { AppShell, Center, Loader } from '@mantine/core';
+import React from 'react';
+import { Link, Route, Routes, Navigate, useLocation } from 'react-router-dom';
+import { AppShell, Burger, Center, Group, Loader, NavLink, ScrollArea, Title } from '@mantine/core';
+import { IconLayoutDashboard, IconTable } from '@tabler/icons-react';
 import { Login } from './pages/Login';
 import { Dashboard } from './pages/Dashboard';
 import { Treatments } from './pages/Treatments';
@@ -21,12 +22,48 @@ function ProtectedRoute({ children }: { children: ReactNode }) {
 }
 
 export default function App() {
+  const [mobileOpened, setMobileOpened] = React.useState(false);
+  const [desktopOpened, setDesktopOpened] = React.useState(true);
+  const location = useLocation();
+
   return (
     <AuthProvider>
-      <AppShell header={{ height: 60 }} padding="md">
+      <AppShell
+        header={{ height: 56 }}
+        navbar={{ width: 260, breakpoint: 'sm', collapsed: { mobile: !mobileOpened, desktop: !desktopOpened } }}
+        padding="md"
+      >
         <AppShell.Header>
-          <AppHeader />
+          <Group h="100%" px="md" justify="space-between">
+            <Group gap="sm">
+              <Burger opened={mobileOpened} onClick={() => setMobileOpened((open) => !open)} hiddenFrom="sm" size="sm" />
+              <Burger opened={desktopOpened} onClick={() => setDesktopOpened((open) => !open)} visibleFrom="sm" size="sm" />
+              <Title order={3}>kalimere:vet</Title>
+            </Group>
+          </Group>
         </AppShell.Header>
+
+        <AppShell.Navbar p="md">
+          <ScrollArea style={{ height: '100%' }}>
+            <NavLink
+              component={Link}
+              to="/"
+              label="Dashboard"
+              leftSection={<IconLayoutDashboard size={16} />}
+              active={location.pathname === '/'}
+              onClick={() => setMobileOpened(false)}
+            />
+            <NavLink
+              component={Link}
+              to="/treatments"
+              label="Treatments"
+              leftSection={<IconTable size={16} />}
+              active={location.pathname.startsWith('/treatments')}
+              onClick={() => setMobileOpened(false)}
+            />
+          </ScrollArea>
+        </AppShell.Navbar>
+
         <AppShell.Main>
           <Routes>
             <Route path="/login" element={<Login />} />
