1cda0dad966ae0d7dca79e8f0f41430613987ddb
Itamar Bar-Lev <bitamar@gmail.com>
2025-10-04 15:30:08 +0300

    fix eslint

---
+++ api/eslint.config.js
@@ -0,0 +1,29 @@
+// @ts-check
+import js from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+import globals from 'globals';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { ignores: ['**/dist/**', '**/node_modules/**'] },
+  js.configs.recommended,
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+      globals: { ...globals.node, ...globals.es2022 },
+    },
+    plugins: { '@typescript-eslint': tseslint },
+    rules: {
+      'no-undef': 'off',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+      '@typescript-eslint/no-explicit-any': 'error',
+    },
+  },
+];
